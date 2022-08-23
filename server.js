50f97a95f5c2a631adce3e26775c1f954786efec
Reggie899 <regina.schmidt1212@gmail.com>
2022-08-23 23:20:20 +0200

    update

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const server = express();
 server.use(cors()); 
 server.use(express.json()); 
 
+let hello = (req, res) => { res.json("läuft")}
+
+server.use('/test', hello)
+
+
+const PORT = 3333;
+
+server.listen(PORT, () => console.log(`Server is listening to port ${PORT} and running`));
